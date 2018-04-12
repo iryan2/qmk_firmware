b603b382986547a5be3fc95d220e78cbfc1af3b5
Ian Ryan <iryan2@gmail.com>
2018-04-12 11:16:44 +1000

    Prevent shift being stuck on when changing layers

--- keyboards/lets_split/keymaps/regal/config.h
+++ keyboards/lets_split/keymaps/regal/config.h
@@ -34,4 +34,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MASTER_RIGHT
 // #define EE_HANDS
 
+// Prevent shift being stuck on when changing layers
+#define PREVENT_STUCK_MODIFIERS
+
 #endif
