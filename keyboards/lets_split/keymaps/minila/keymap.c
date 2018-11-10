0e5edd7919f19b662a0bff11aeb9d86cb1adc6ff
Ian Ryan <iryan2@gmail.com>
2018-11-10 11:45:51 +1000

    Remove lines in keymap relating to `KC_LEAD`
    (leader key),
    makes build fail

--- keyboards/lets_split/keymaps/minila/keymap.c
+++ keyboards/lets_split/keymaps/minila/keymap.c
@@ -31,9 +31,6 @@ enum custom_keycodes {
 #define CMD_LSB LGUI(KC_LBRACKET)
 #define CMD_RSB RGUI(KC_RBRACKET)
 
-#undef LEADER_TIMEOUT
-#define LEADER_TIMEOUT 300
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
@@ -44,14 +41,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |Enter |Leader|  Alt |  Cmd |Lower |Space |Space |Raise | Cmd  | Alt  | Ctrl |Enter |
+ * |Enter |      |  Alt |  Cmd |Lower |Space |Space |Raise | Cmd  | Alt  | Ctrl |Enter |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT( \
   KC_TAB,  KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_BSPC, \
   CTL_ESC, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT, \
   KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT , \
-  KC_ENT,  KC_LEAD, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RGUI, KC_RALT, KC_RCTRL, KC_ENT \
+  KC_ENT,  _______, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RGUI, KC_RALT, KC_RCTRL, KC_ENT \
 ),
 
 /* Lower
@@ -201,25 +198,3 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 
-LEADER_EXTERNS();
-
-void matrix_scan_user(void) {
-  LEADER_DICTIONARY() {
-    leading = false;
-    leader_end();
-
-    SEQ_ONE_KEY(KC_E) {
-      // Open blank text editor (TextEdit.app via spotlight)
-      SEND_STRING(SS_LGUI(" ")"textedit"SS_TAP(X_ENTER));
-    }
-    SEQ_ONE_KEY(KC_V) {
-      // Open last ag results in vim
-      SEND_STRING("vim $(!! -l)"SS_TAP(X_ENTER)SS_TAP(X_ENTER));
-    }
-    SEQ_ONE_KEY(KC_C) {
-      // Copy visual selection in emacs to system clipboard
-      // Right alt + |, 'pbcopy', enter
-      SEND_STRING(SS_RALT("|")"pbcopy"SS_TAP(X_ENTER));
-    }
-  }
-}
