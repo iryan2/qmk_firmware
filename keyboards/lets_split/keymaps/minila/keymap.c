751f5af7f1ba0198177f4025ee31e505e54cf7fe
Ian Ryan <iryan2@gmail.com>
2018-11-13 20:13:58 +1000

    Home -> Cmd+Left, End -> Cmd+Right

--- keyboards/lets_split/keymaps/minila/keymap.c
+++ keyboards/lets_split/keymaps/minila/keymap.c
@@ -30,6 +30,8 @@ enum custom_keycodes {
 #define CTL_ESC CTL_T(KC_ESC)
 #define CMD_LSB LGUI(KC_LBRACKET)
 #define CMD_RSB RGUI(KC_RBRACKET)
+#define CMD_LFT RGUI(KC_LEFT)
+#define CMD_RGT RGUI(KC_RGHT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -47,8 +49,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT( \
   KC_TAB,  KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_BSPC, \
   CTL_ESC, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT, \
-  KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT , \
-  KC_ENT,  _______, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RGUI, KC_RALT, KC_RCTRL, KC_ENT \
+  KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
+  KC_ENT,  _______,KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RGUI, KC_RALT, KC_RCTRL, KC_ENT \
 ),
 
 /* Lower
@@ -64,8 +66,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT( \
   KC_GRV,   _______, _______, KC_UP,   _______, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, KC_DEL, \
-  KC_LCTRL, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_EQL,  KC_MINS, KC_HOME, KC_PGUP, KC_BSPC, KC_BSLS, \
-  KC_LSFT,  DEV_ON,  _______, _______, _______, _______, _______, KC_DEL,  KC_END,  KC_PGDN, _______, KC_ENT, \
+  KC_LCTRL, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_EQL,  KC_MINS, CMD_LFT, KC_PGUP, KC_BSPC, KC_BSLS, \
+  KC_LSFT,  DEV_ON,  _______, _______, _______, _______, _______, KC_DEL,  CMD_RGT,  KC_PGDN, _______, KC_ENT, \
   _______,  _______, KC_LALT, KC_LGUI, _______, _______, _______, _______, _______, _______, _______, _______ \
 ),
 
